919de0a37235a62749b772bf943a717b3902b54f
danawyeth <danawyeth@proton.me>
2022-11-09 10:41:08 +0800

    Fetch handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,78 +1,131 @@
-import * as ActionTypes from './ActionTypes';
-import { DISHES } from '../shared/dishes';
-import { baseUrl } from '../shared/baseUrl';
-import { comment } from 'postcss';
+import * as ActionTypes from "./ActionTypes";
+import { DISHES } from "../shared/dishes";
+import { baseUrl } from "../shared/baseUrl";
+import { comment } from "postcss";
 
 export const addComment = (dishId, rating, author, comment) => ({
-     type: ActionTypes.ADD_COMMENT,
-     payload: {
-        dishId: dishId,
-        rating: rating,
-        author: author,
-        comment: comment
-     }
-})
+  type: ActionTypes.ADD_COMMENT,
+  payload: {
+    dishId: dishId,
+    rating: rating,
+    author: author,
+    comment: comment,
+  },
+});
 //thunk
 export const fetchDishes = () => (dispatch) => {
-    dispatch(dishesLoading(true));
+  dispatch(dishesLoading(true));
+  //setTimeout(() => {
+  //    dispatch(addDishes(DISHES));
+  //}, 2000)
 
-    //setTimeout(() => {
-    //    dispatch(addDishes(DISHES));
-    //}, 2000)
-
-    return fetch(baseUrl + 'dishes') // to fetch the dishes url
-        .then(response => response.json()) //handles promise that is returned
-        .then(dishes => dispatch(addDishes(dishes))); //  takes the json
-}
+  return fetch(baseUrl + 'dishses') // to fetch the dishes url
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error" + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        var errmess = new Error(error.message);
+        throw errmess; // handles if server doesn't give back anything
+      }
+    )
+    .then((response) => response.json()) //handles promise that is returned
+    .then((dishes) => dispatch(addDishes(dishes))) //  takes the json
+    .catch((error) => dispatch(dishesFailed(error.message)));
+};
 
 export const dishesLoading = () => ({
-    type: ActionTypes.DISHES_LOADING
+  type: ActionTypes.DISHES_LOADING,
 });
 
 export const dishesFailed = (errmess) => ({
-    type: ActionTypes.DISHES_FAILED,
-    payload: errmess
+  type: ActionTypes.DISHES_FAILED,
+  payload: errmess,
 });
 
 export const addDishes = (dishes) => ({
-    type: ActionTypes.ADD_DISHES,
-    payload: dishes
-}); 
+  type: ActionTypes.ADD_DISHES,
+  payload: dishes,
+});
 
 export const fetchComments = () => (dispatch) => {
-    return fetch(baseUrl + 'comments') // to fetch the dishes url
-        .then(response => response.json()) //handles promise that is returned
-        .then(comments => dispatch(addDishes(comments))); //  takes the json file and dispatch it in to the 
-}
+  return fetch(baseUrl + "comments") // to fetch the dishes url
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error" + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        var errmess = new Error(error.message);
+        throw errmess; // handles if server doesn't give back anything
+      }
+    )
+    .then((response) => response.json()) //handles promise that is returned
+    .then((comments) => dispatch(addComments(comments))) //  takes the json file and dispatch it in to the
+    .catch((error) => dispatch(commentsFailed(error.message)));
+};
 
 export const commentsFailed = (errmess) => ({
-    type: ActionTypes.COMMENTS_FAILED,
-    payload: errmess
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: errmess,
 });
 
 export const addComments = (comments) => ({
-    type: ActionTypes.ADD_COMMENTS,
-    payload: comments
-}); 
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments,
+});
 
 export const fetchPromos = () => (dispatch) => {
-    dispatch(promosLoading(true));
+  dispatch(promosLoading(true));
 
-    return fetch(baseUrl + 'promotions') // to fetch the dishes url
-        .then(response => response.json()) //handles promise that is returned
-        .then(promos => dispatch(addPromos(promos))); //  takes the json
-}
+  return fetch(baseUrl + "promotions") // to fetch the dishes url
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error" + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        var errmess = new Error(error.message);
+        throw errmess; // handles if server doesn't give back anything
+      }
+    )
+    .then((response) => response.json()) //handles promise that is returned
+    .then((promos) => dispatch(addPromos(promos))) //  takes the json
+    .catch((error) => dispatch(promosFailed(error.message)));
+};
 
 export const promosLoading = () => ({
-    type: ActionTypes.PROMOS_LOADING
+  type: ActionTypes.PROMOS_LOADING,
 });
 
 export const promosFailed = (errmess) => ({
-    type: ActionTypes.PROMOS_FAILED,
-    payload: errmess
+  type: ActionTypes.PROMOS_FAILED,
+  payload: errmess,
 });
 
 export const addPromos = (promos) => ({
-    type: ActionTypes.ADD_PROMOS,
-    payload: promos
-}); 
+  type: ActionTypes.ADD_PROMOS,
+  payload: promos,
+});
